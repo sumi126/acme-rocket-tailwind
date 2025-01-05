ad50a96ad1b8188374af743cb8205712a472f4bf
Imratuzzahan Sumi <imratuzzahansumi@gmail.com>
2025-01-05 07:55:21 +0600

    Manually Create hamburger button and mobile meno icon and add animation while toggle

--- build/js/main.js
+++ build/js/main.js
@@ -5,8 +5,11 @@ const initApp = () => {
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
+        hamburgerBtn.classList.toggle('toggle-btn')
     }
+
     hamburgerBtn.addEventListener('click',toggleMenu )
     mobileMenu.addEventListener('click',toggleMenu )
 }
+
 document.addEventListener('DOMContentLoaded',initApp)
